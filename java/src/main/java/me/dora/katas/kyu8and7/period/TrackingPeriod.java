aac41f90e8514c3fc1e6b4f8666a1ff7d94f0549
Dora Sasdi <sasdi.dori@gmail.com>
2025-01-11 19:33:24 +0100

    add 8kyu kata isPeriodLate

---
+++ java/src/main/java/me/dora/katas/kyu8and7/period/TrackingPeriod.java
@@ -0,0 +1,13 @@
+package me.dora.katas.kyu8and7.period;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+
+public class TrackingPeriod {
+    public boolean periodIsLate(LocalDate last, LocalDate today, int cycleLength) {
+        long until = last.until(today, ChronoUnit.DAYS);
+        int daysSinceLast = Long.valueOf(until).intValue();
+        return daysSinceLast > cycleLength;
+    }
+}
